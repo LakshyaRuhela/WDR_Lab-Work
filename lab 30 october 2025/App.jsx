ed6e83881abc669e1693a8bca35ad3b761ac0dde
Lakshya Ruhela <ruhelalakshya3@gmail.com>
2025-10-30 12:44:21 +0530

    Delete lab 30 october 2025 directory

--- lab 30 october 2025/App.jsx
+++
@@ -1,9 +0,0 @@
-import Faculty from "./Faculty.jsx";
-function App(){
-  return(
-    <div>
-      <Faculty facultyName = "Ramesh" subject = "Java" experience="13 years"/>
-    </div>
-  )
-}
-export default App;
